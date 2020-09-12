d5852620060b1c8e1f1c0af7f684940c9cb8f9eb
Yabuoshi Keisuke <yabuoshikeisuke@team-lab.com>
2020-09-12 11:35:09 +0900

    [mod] change module settings

--- leetcode/src/Leetcode.kt
+++ leetcode/src/Leetcode.kt
@@ -2,10 +2,10 @@ fun main(args: Array<String>) {
     // val x = readLine()!!.split(',').map { it.toInt() }.toIntArray()
     // val x = readLine()!!.toInt()
 
-    val x = readLine()!!.toInt()
+    val x = readLine()!!.split(',').map { it.toInt() }.toIntArray()
 
     val sol = Solution()
-    val ans = sol.isHappy(x)
+    val ans = sol.maxProfit(x)
 
     println(ans.toString())
 }
--- leetcode/src/Solution.kt
+++ leetcode/src/Solution.kt
@@ -1,25 +1,5 @@
 class Solution {
-    fun isHappy(n: Int): Boolean {
-        var tmp = n.toLong()
-        val apperred = mutableSetOf<Long>()
+    fun maxProfit(prices: IntArray): Int {
 
-        while (!apperred.contains(tmp)) {
-            if (tmp == 1L) return true
-            apperred.add(tmp)
-            tmp = calcHappy(tmp)
-            println(tmp)
-        }
-
-        return false
-    }
-
-    fun calcHappy(n: Long): Long {
-        var ret = 0L
-        var tmp = n
-        while (tmp > 0) {
-            ret += (tmp % 10) * (tmp % 10)
-            tmp /= 10
-        }
-        return ret
     }
 }
