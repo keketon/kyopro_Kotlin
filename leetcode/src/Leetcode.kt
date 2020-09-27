bcee4b2dd4c0616539de2dd7fd099bdf566fe004
Yabuoshi Keisuke <yabuoshikeisuke@team-lab.com>
2020-09-27 15:44:50 +0900

    [add] (Leetcode) calculation process time and compatible multi-testcases

--- leetcode/src/Leetcode.kt
+++ leetcode/src/Leetcode.kt
@@ -1,20 +1,30 @@
-import java.io.BufferedReader
 import java.io.File
-import java.io.InputStreamReader
-import java.lang.Exception
 import java.util.*
 
 fun main(args: Array<String>) {
     val TEST_CASE_FILE_NAME = "leetcode/resources/testCase.txt"
     val br = File(TEST_CASE_FILE_NAME).bufferedReader()
 
-//    val x = br.readLine()!!.split(',').map { it.toInt() }.toIntArray()
-//    val x = br.readLine()!!.toInt()
 
-    val x = br.readLine()!!.toInt()
+    while (true) {
+        val startTime = System.currentTimeMillis()
 
-    val sol = Solution()
-    val ans = sol.xxx(x)
+        // ---process start---
+        val input = br.readLine() ?: break
 
-    println(ans.toString())
+//        val x = input.split(',').map { it.toInt() }.toIntArray()
+//        val x = input.toInt()
+        val x = input.toInt()
+
+        val sol = Solution()
+        val ans = sol.xxx(x)
+
+        println(ans.toString())
+        // ---process end---
+
+        val endTime = System.currentTimeMillis()
+        val processTime = endTime - startTime
+
+        println("Completed in $processTime ms")
+    }
 }
